604369397bdb6bccffb3eee40d7c943e7c0f9ce0
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-01-26 13:01:06 -0600

    to do list skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+
+      <form autoComplete="off">
+        <input type="text" name="todos" id="todos"
+        required placeholder="What needs to be done?" />
+        <button type="submit">Create</button>
+      </form>
+
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" id="" />
+            Cooking To Do
+          </label>
+          <button>Edit</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" id="" /> 
+            Cooking To Do 
+          </label>
+          <button>Edit</button>
+        </li>
+      </ul>
+
+      <div className="row">
+        <label htmlFor="all">
+          <input type="checkbox" name="all" id="all" />
+            ALL
+        </label>
+        <p>You have 0 to do</p>
+        <button id="delete">Delete</button>
+      </div>
     </div>
   );
 }
