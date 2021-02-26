f22e177e343aef4ec0c69a4123af2c0affd56465
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-25 20:25:47 -0600

    add local storage

--- src/Components/DataProvider.js
+++ src/Components/DataProvider.js
@@ -6,6 +6,11 @@ export const DataProvider = (props) => {
     const [todos, setTodos] = useState([])
 
     useEffect (() => {
+        const todoStore = JSON.parse(localStorage.getItem('todoStore'))
+        if(todoStore) setTodos(todoStore)
+    },[])
+
+    useEffect (() => {
         localStorage.setItem('todoStore', JSON.stringify(todos))
     }, [todos])
 
