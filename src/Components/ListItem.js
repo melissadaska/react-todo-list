145f11ac12938b9272c8b431742392efc64cd3a1
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-25 20:49:04 -0600

    add edit feature

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function ListItem({todo, id, checkComplete}) {
-    return (
-        <div>
-            <li>
-                <label htmlFor={id} className={todo.complete ? "active" : ""}>
-                    <input type="checkbox" id={id} checked={todo.complete}
-                    onChange={() => checkComplete(id)}/>
-                    {todo.name}
-                </label>
-                <button disabled={todo.complete}>Edit</button>
-            </li>
-        </div>
-    )
+    const [onEdit, setOnEdit] = useState(false)
+    const [editValue, setEditValue] = useState(todo.name)
+
+    const handleOnEdit = () => {
+        setOnEdit(true)
+    }
+
+    const handleSave = id => {
+        setOnEdit(false)
+    }
+
+    if(onEdit) {
+        return (
+            
+                <li>
+                    
+                    <input type="text" id="editValue" value={editValue} name="editValue" 
+                    onChange={e => setEditValue(e.target.value.toLowerCase())}/>
+                    <button onClick={() => handleSave(id)}>Save</button>
+                </li>
+        )
+    } else {
+        return (
+            <div>
+                <li>
+                    <label htmlFor={id} className={todo.complete ? "active" : ""}>
+                        <input type="checkbox" id={id} checked={todo.complete}
+                        onChange={() => checkComplete(id)}/>
+                        {todo.name}
+                    </label>
+                    <button disabled={todo.complete} onClick={handleOnEdit}>Edit</button>
+                </li>
+            </div>
+        ) 
+    }
 }
