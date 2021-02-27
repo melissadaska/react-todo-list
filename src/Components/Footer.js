2afdab848b5a99714a7be352f0cc4e7d4dca9277
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-26 20:01:38 -0600

    add checkall

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {DataContext} from './DataProvider';
 
 export default function Footer() {
+    const [checkAll, setCheckAll] = useState(false)
+    const [todos, setTodos] = useContext(DataContext)
+
+    const handleCheckAll = () => {
+        const newTodos = [...todos]
+        newTodos.forEach(todo => {
+            todo.complete = !checkAll
+        })
+        setTodos(newTodos)
+        setCheckAll(!checkAll)
+    }
+
     return (
         <div className="row">
             <label htmlFor="all">
-                <input type="checkbox" name="all" id="all" />
+                <input type="checkbox" name="all" id="all" 
+                onClick={handleCheckAll} checked={checkAll}/>
                 ALL
             </label>
-            <p>You have 0 to do</p>
+            <p>You have {todos.length} to do</p>
             <button id="delete">Delete</button>
         </div>
     )
